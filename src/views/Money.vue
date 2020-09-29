f41f229c222b57e78928b994d245aea996d51efa
yake go <cazser>
2020-09-29 09:25:03 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Money
+    <div class="nav-wrapper">
+      <div class="content">
+            Money
+          </div>
         <Nav/>
     </div>
 </template>
@@ -14,5 +16,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        border: 1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        flex-grow: 1;
+        overflow: auto;
+    }
 </style>
