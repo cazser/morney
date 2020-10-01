6f37605c095c919b4ef0818660d4a6d5377fc5ac
yake go <cazser>
2020-10-01 16:32:04 +0800

    money页面模块化

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <label class="notes">
+            <span class="name">备注</span>
+            <input class="value" type="text"
+             placeholder="在这里输入备注">
+        </label>
+    </div>
+    
+</template>
+
+<script lang="ts">
+    export default{
+        name:"Notes"
+    }
+</script>
+
+<style lang="scss" scoped>
+        @import "~@/assets/style/helper.scss";
+        @import "~@/assets/style/reset.scss";
+        .notes{
+        background: #f5f5f5;
+        font-size: 14px;
+        display: block;
+        padding: 0 16px;
+        display: flex;
+        align-items: center;
+        .name{
+            padding-right: 16px;
+        }
+        input{
+            height: 64px;
+            flex-grow: 1;
+            background: transparent;
+            border: none;        
+        }
+        > input:focus {
+        outline:none;
+        border: 1px solid #4e6ef2;
+        background:white;
+        }   
+    }
+</style>
