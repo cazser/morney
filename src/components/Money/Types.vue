4f6812a2f917a55eb40ac4b01482d5819b2519d8
yake go <cazser>
2020-10-02 12:48:43 +0800

    完成了Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -28,7 +28,7 @@
     }
 */
     import Vue from 'vue';
-    import {Component} from 'vue-property-decorator';
+    import {Component, Prop} from 'vue-property-decorator';
     @Component
     export default class Types extends Vue{
         type='-'  //-表示支出，+表示收入
